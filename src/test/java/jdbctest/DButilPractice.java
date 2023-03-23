d9aaa8e5684a7e99aca226a9b81b9f99290f1e87
İlyar YAGASPURT <ilyar98@hotmail.com>
2023-03-23 13:47:00 +0300

    DBUtils_practice

--- src/test/java/jdbctest/DButilPractice.java
+++ src/test/java/jdbctest/DButilPractice.java
@@ -1,4 +1,54 @@
 package jdbctest;
 
+import org.junit.jupiter.api.Test;
+import utilities.DBUtils;
+
+import java.util.List;
+import java.util.Map;
+
 public class DButilPractice {
+
+    @Test
+    public void test1(){
+
+        //create connection
+        DBUtils.createConnection();
+        String query = "select FIRST_NAME,LAST_NAME,SALARY,JOB_ID\n" +
+                "from EMPLOYEES\n" +
+                "where ROWNUM<6";
+        List<Map<String, Object>> queryData = DBUtils.getQueryResultMap(query);
+
+        //print the result
+        for (Map<String, Object> row : queryData) {
+            System.out.println(row.toString());
+        }
+
+        //close the connection
+        DBUtils.destroy();
+
+    }
+
+    @Test
+    public void test2(){
+
+        //create connection
+        DBUtils.createConnection();
+        String query = "select FIRST_NAME,LAST_NAME,SALARY,JOB_ID\n" +
+                "from EMPLOYEES\n" +
+                "where ROWNUM<2";
+
+        Map<String, Object> rowMap = DBUtils.getRowMap(query);
+
+        //print the result
+
+        System.out.println(rowMap.toString());
+
+
+        //close the connection
+        DBUtils.destroy();
+
+    }
+
+
+
 }
